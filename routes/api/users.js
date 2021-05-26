86520833d9030bce4c2d6959bdbba427accca6a1
Gerry M <waltermccone88@gmail.com>
2021-05-26 17:18:07 -0500

    add get and put routes to the route and controller for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,3 +5,10 @@ const userController = require("../../controllers/userController");
 router
   .route("/")
   .post(userController.create);
+
+  router
+  .route("/:id")
+  .get(userController.findById)
+  .put(userController.update);
+
+module.exports = router;
