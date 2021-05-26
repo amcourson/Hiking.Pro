3137406b086de57ffa6b318487167aa641ec5ae4
Gerry M <waltermccone88@gmail.com>
2021-05-26 17:11:33 -0500

    updated the index and the user route files in api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const userController = require("../../controllers/userController");
+const userRoutes = require("./users");
 
+// Post routes
+router.use("/users", userRoutes);
 
-router
-  .route("/")
-  .post(userController.create);
+module.exports = router;
