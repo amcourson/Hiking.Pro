6d99363abeae44802ca9bba79fa80ffb96ec4a4d
Gerry M <waltermccone88@gmail.com>
2021-05-26 20:47:02 -0500

    updated user model to contain location and added our plan deliverable

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
   username: { type: String, required: true, match: [/^[a-zA-Z0-9]+$/, 'is invalid'] },
   email: { type: String, required: [true, "Can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'] },
   password: {type: String, required: [true, "Can't be blank"], match: [/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, 'must be atleast 8 characters, atleast 1 letter and 1 number']},
+  location: {type: String, required: [true, "Can't be blank"]}, match: [/^[a-zA-Z]+$/, 'is invalid'],
   points: {type: Number, default: 0}
 });
 
