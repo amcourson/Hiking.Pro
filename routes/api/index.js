49eed68ce7748f09460ff2114b2298ef068ebdd3
Gerry M <waltermccone88@gmail.com>
2021-05-30 16:29:37 -0500

    create location api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const locationRoutes = require("./locations");
 
 // Post routes
 router.use("/users", userRoutes);
+router.use("/locations", locationRoutes);
 
 
 module.exports = router;
