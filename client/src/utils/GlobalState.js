e348bc693d2fe2af2d57231b404e43b5dc0a4ce3
Walter M <waltermccone88@gmail.com>
2021-06-04 20:10:41 -0500

    fixed the set search location action in the state manager

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -17,7 +17,7 @@ const reducer = (state, action) => {
         case SET_SEARCH_LOCATION:
             return {
               ...state,
-              searchLocation: action.location,
+              searchLocation: action.region,
               loading: false
             };
 
