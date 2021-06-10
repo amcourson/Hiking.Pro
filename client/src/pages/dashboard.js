fdceaefced788c8c8810a6387b0eef6fee184df3
Angelica Marie Courson <74441968+amcourson@users.noreply.github.com>
2021-06-09 20:58:39 -0500

    Revert "made some layout tweaks to the trail page and dashboard"

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -11,17 +11,11 @@ const Dashboard = () => {
         <div>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8">
-                        <CreateLocationForm />
-                    </div>
-                </div>
-                <div className="row">
                     <div className="col-md-6">
                         <UserInfo />
                     </div>
                     <div className="col-md-6">
-
+                        <CreateLocationForm />
                         <LocationList />
                     </div>
                 </div>
